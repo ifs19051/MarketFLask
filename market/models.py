498484d98bcfb14ce79fdfb85d3cd8e766eb6520
Jogi <stigmahotspot@gmail.com>
2022-06-01 11:17:41 +0700

    commit
    halo

--- market/models.py
+++ market/models.py
@@ -17,9 +17,9 @@ class User(db.Model, UserMixin):
     @property
     def prettier_budget(self):
         if len(str(self.budget)) >= 4:
-            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+            return f'Rp{str(self.budget)[:-3]},{str(self.budget)[-3:]}'
         else:
-            return f"{self.budget}$"
+            return f"Rp{self.budget}"
 
     @property
     def password(self):
